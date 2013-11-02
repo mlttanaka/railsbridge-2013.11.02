2bf10908f0b43c5e9b91328908dfb71c46d4cb69
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-11-02 10:24:09 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HowAboutPurple::Application.config.secret_key_base = '756d8c289779d84fb8f2ede32b8d9cf35eaeca2ea0117d443f9103eb78168559a75fae52720fc6678bea7e4bdc772a06e056e7b407f850147a8c28931f0477f6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HowAboutPurple::Application.config.session_store :cookie_store, key: '_HowAboutPurple_session'
