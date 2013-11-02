d55dcece4daf5243f7e7a9eb178766ae4de2343f
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-11-02 13:11:02 -0700

    Add logout link to application layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 HowAboutPurple::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  devise_for :users
 
-  # You can have the root of your site routed with "root"
   root 'home#index'
 
   # Example of regular route:
