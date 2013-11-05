8340fd3c89d2096c01575faf589051880fe433fc
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-11-05 11:50:34 -0800

    Add link to post from Post#index.

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,5 +1,5 @@
 class Reply < ActiveRecord::Base
-  validates :content, presence: true, length: { maximum: 20 }
+  validates :content, presence: true, length: { maximum: 200 }
   belongs_to :post
   belongs_to :user
 end
