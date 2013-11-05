0a87727e35c1b4a5e8a7cbe297ae8fca5f9dfd86
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-11-05 08:55:01 -0800

    Reformat AM/PM as am/pm.

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,2 +1,2 @@
-Time::DATE_FORMATS[:month_day_year_12hr_time] = '%B %e, %Y %l:%M%p'
+Time::DATE_FORMATS[:month_day_year_12hr_time] = '%B %e, %Y %l:%M%P'
 Time::DATE_FORMATS[:mon_day_year] = '%b %e, %Y'
