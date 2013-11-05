c717627dccca599c95db28d18f276fd90fb3e220
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2013-11-05 08:42:15 -0800

    Correct 'new' method in Replies controller.  Pretty up the views.

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -3,21 +3,21 @@ class RepliesController < ApplicationController
   def new
     @post = current_user.posts.find(params[:post_id])
     @title = @post.title
-    @reply = @post.replies.new
   end
   
   def create
     @post = Post.find(params[:post_id])
-    @reply = Reply.new(reply_params)
-    @reply.post = @post
-    @reply.user = current_user
+    @reply = @post.replies.new(reply_params)
+    @reply.user_id = current_user.id
+    @reply.save
     
     if @reply.save
       redirect_to post_path(@post)
     else
-      render 'replies#new'
+      render 'new'
     end
   end
+  
   
   private
   
